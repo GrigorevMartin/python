b460f27347911464c0a0bf1f7fff785abbf40b3c
GrigorevMartin <159944451+GrigorevMartin@users.noreply.github.com>
2024-03-08 12:45:02 +0200

    Add files via upload

---
+++ 23.py
@@ -0,0 +1,3 @@
+numbers = [6, 6, 7, 5, 6, 4]
+for i in range(len(numbers)):
+    print(numbers[i])
---
+++ 24.py
@@ -0,0 +1,5 @@
+numbers = [6, 6, 7, 5, 6, 4]
+if 7 in numbers:
+    print("есть число 7")
+else:
+    print("числа 7 нет в массиве")
